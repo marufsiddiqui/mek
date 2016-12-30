21454538f841fedff58a533bf3ed9536b72c0949
Maruf Siddiqui <maruf.siddiqui@shore.com>
2016-12-30 10:36:31 +0100

    load Pilots from sample data

--- src/app/reducers/entitiesReducer.js
+++ src/app/reducers/entitiesReducer.js
@@ -1,7 +1,18 @@
-import {createReducer} from "common/utils/reducerUtils";
+import { createReducer } from "common/utils/reducerUtils";
 import schema from 'app/schema';
+import { DATA_LOADED } from 'features/tools/toolConstants';
 
 const initialState = schema.getDefaultState();
 
+export function loadData(state, { pilots }) {
+  const session = schema.from(state);
+  const { Pilot } = session;
+
+  pilots.forEach(Pilot.parse);
+
+  return session.reduce();
+}
+
 export default createReducer(initialState, {
+  [DATA_LOADED]: loadData,
 });
